095f2a60bc5bf35d14533705b498286100dfeaa0
Tomas Tornamira <103047119+TTorna@users.noreply.github.com>
2022-05-03 22:12:26 -0300

    Delete MySQL directory

--- MySQL/Insert_Perfil.sql
+++
@@ -1,9 +0,0 @@
-Use BDC;
-
-Insert into BDC.Perfil(Personalidad,Estrategia) 
-Values ("Activo","Se tiene en cuenta que la prediccion puede ser arriesgada");
-
-Insert into BDC.Perfil(Personalidad,Estrategia) 
-Values ("Pasivo", "Se tiene en cuenta que la prediccion es poco arriesgada");
-
-Select * From Perfil;
--- MySQL/Insert_Roles.sql
+++
@@ -1,7 +0,0 @@
-Use BDC;
-
-Insert into BDC.Roles(Tipo,Descripcion) 
-Values (1,"Usuario: Actor el cual puede usar los servicios basicos del sistema");
-
-Insert into BDC.Roles(Tipo, Descripcion)
-Values (2, "Admin: Actor el cual puede Ajustar los Indicadores")
