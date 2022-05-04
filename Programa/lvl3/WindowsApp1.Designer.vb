2c8896ad2c49400576b46ce45762b80e8eea0fe3
Tomas Tornamira <103047119+TTorna@users.noreply.github.com>
2022-05-03 22:23:52 -0300

    Add files via upload

---
+++ Programa/lvl3/WindowsApp1.Designer.vb
@@ -0,0 +1,261 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TxtMail = New System.Windows.Forms.TextBox()
+        Me.TxtNom = New System.Windows.Forms.TextBox()
+        Me.TxtCtñ = New System.Windows.Forms.TextBox()
+        Me.TxtApel = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.BttSesion = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TxtMail
+        '
+        Me.TxtMail.Location = New System.Drawing.Point(222, 91)
+        Me.TxtMail.Name = "TxtMail"
+        Me.TxtMail.Size = New System.Drawing.Size(149, 20)
+        Me.TxtMail.TabIndex = 0
+        '
+        'TxtNom
+        '
+        Me.TxtNom.Location = New System.Drawing.Point(222, 129)
+        Me.TxtNom.Name = "TxtNom"
+        Me.TxtNom.Size = New System.Drawing.Size(149, 20)
+        Me.TxtNom.TabIndex = 1
+        '
+        'TxtCtñ
+        '
+        Me.TxtCtñ.Location = New System.Drawing.Point(222, 206)
+        Me.TxtCtñ.Name = "TxtCtñ"
+        Me.TxtCtñ.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TxtCtñ.Size = New System.Drawing.Size(149, 20)
+        Me.TxtCtñ.TabIndex = 3
+        '
+        'TxtApel
+        '
+        Me.TxtApel.Location = New System.Drawing.Point(222, 167)
+        Me.TxtApel.Name = "TxtApel"
+        Me.TxtApel.Size = New System.Drawing.Size(149, 20)
+        Me.TxtApel.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Unispace", 9.749999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(56, 170)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(143, 15)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Ingrese apellido:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Unispace", 9.749999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(92, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(111, 15)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Ingrese mail:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Unispace", 9.749999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(74, 132)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(127, 15)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Ingrese nombre:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Unispace", 9.749999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(38, 209)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(159, 15)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Ingrese contraseña:"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.ActiveLinkColor = System.Drawing.Color.Red
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.BackColor = System.Drawing.Color.Transparent
+        Me.LinkLabel1.Font = New System.Drawing.Font("Unispace", 9.749999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel1.LinkColor = System.Drawing.Color.MediumSpringGreen
+        Me.LinkLabel1.Location = New System.Drawing.Point(335, 247)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(39, 15)
+        Me.LinkLabel1.TabIndex = 9
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "aquí"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.Transparent
+        Me.Label6.Font = New System.Drawing.Font("Unispace", 9.749999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.White
+        Me.Label6.Location = New System.Drawing.Point(38, 247)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(295, 15)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "En caso de no tener cuenta haz click"
+        '
+        'BttSesion
+        '
+        Me.BttSesion.BackColor = System.Drawing.Color.White
+        Me.BttSesion.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.BttSesion.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.25!)
+        Me.BttSesion.Location = New System.Drawing.Point(109, 307)
+        Me.BttSesion.Name = "BttSesion"
+        Me.BttSesion.Size = New System.Drawing.Size(221, 37)
+        Me.BttSesion.TabIndex = 11
+        Me.BttSesion.Text = "Iniciar sesion"
+        Me.BttSesion.UseVisualStyleBackColor = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.Color.Transparent
+        Me.Label7.Font = New System.Drawing.Font("Unispace", 26.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.SpringGreen
+        Me.Label7.Location = New System.Drawing.Point(155, 23)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(123, 42)
+        Me.Label7.TabIndex = 19
+        Me.Label7.Text = "Login"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.BackgroundImage = Global.WindowsApp1.My.Resources.Resources.cerrar
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox1.Location = New System.Drawing.Point(382, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(28, 27)
+        Me.PictureBox1.TabIndex = 21
+        Me.PictureBox1.TabStop = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Unispace", 8.999999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(38, 274)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(301, 14)
+        Me.Label5.TabIndex = 24
+        Me.Label5.Text = "En caso de olvidar su contraseña haz click"
+        Me.Label5.Visible = False
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.ActiveLinkColor = System.Drawing.Color.Red
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.BackColor = System.Drawing.Color.Transparent
+        Me.LinkLabel2.Font = New System.Drawing.Font("Unispace", 8.999999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel2.LinkColor = System.Drawing.Color.MediumSpringGreen
+        Me.LinkLabel2.Location = New System.Drawing.Point(339, 274)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(35, 14)
+        Me.LinkLabel2.TabIndex = 23
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "aquí"
+        Me.LinkLabel2.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BackColor = System.Drawing.Color.Black
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(422, 355)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.LinkLabel2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.BttSesion)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TxtApel)
+        Me.Controls.Add(Me.TxtCtñ)
+        Me.Controls.Add(Me.TxtNom)
+        Me.Controls.Add(Me.TxtMail)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Login"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TxtMail As TextBox
+    Friend WithEvents TxtNom As TextBox
+    Friend WithEvents TxtCtñ As TextBox
+    Friend WithEvents TxtApel As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Label6 As Label
+    Friend WithEvents BttSesion As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents LinkLabel2 As LinkLabel
+End Class
---
+++ Programa/lvl3/WindowsApp1.vb
@@ -0,0 +1,65 @@
+﻿Public Class Form1
+    Private Arrastrar As Boolean = False
+    Private MouseDownX As Integer
+    Private MouseDownY As Integer
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Label5.Visible = False
+        LinkLabel2.Visible = False
+        Me.Hide()
+        PerfilUser.Show()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BttSesion.Click
+        Dim vUsuario = New Usuario()
+        If vUsuario.LoguearAdmin(TxtMail.Text, TxtNom.Text, TxtApel.Text, TxtCtñ.Text) = 0 Then
+            If vUsuario.Loguear(TxtMail.Text, TxtNom.Text, TxtApel.Text, TxtCtñ.Text) = 0 Then
+                MsgBox("Datos incorrectos, por favor introduzca los correctos")
+                Label5.Visible = True
+                LinkLabel2.Visible = True
+            Else
+                MsgBox("los datos se ingresaron correctamente", MsgBoxStyle.Information, "Insertar Datos")
+                Me.Hide()
+                Interfaz_Intermedia.Show()
+            End If
+        Else
+            MsgBox("los datos se ingresaron correctamente", MsgBoxStyle.Information, "Insertar Datos")
+            Me.Hide()
+            Interfaz_Admin.Show()
+        End If
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Me.Close()
+    End Sub
+    Private Sub F_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs) Handles Me.MouseDown
+        If e.Button = MouseButtons.Left Then
+            Arrastrar = True
+            MouseDownX = e.X
+            MouseDownY = e.Y
+        End If
+    End Sub
+
+    Private Sub F_MouseUp(ByVal sender As Object, ByVal e As MouseEventArgs) Handles MyBase.MouseUp
+        If e.Button = MouseButtons.Left Then
+            Arrastrar = False
+        End If
+    End Sub
+
+    Private Sub F_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs) Handles MyBase.MouseMove
+        If Arrastrar = True Then
+            Dim temp As Point = New Point()
+            temp.X = Me.Location.X + (e.X - MouseDownX)
+            temp.Y = Me.Location.Y + (e.Y - MouseDownY)
+            Me.Location = temp
+            temp = Nothing
+        End If
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        Label5.Visible = False
+        LinkLabel2.Visible = False
+        Me.Hide()
+        ActualizadorDeUsuario.Show()
+    End Sub
+End Class
